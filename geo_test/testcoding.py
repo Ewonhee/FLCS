5ea8df7053eb33acfb5f3394ac07abc951814179
ewonhee <ii6725@naver.com>
2022-05-18 16:58:17 +0900

    지오코더 구글api사용 테스트코드 입니다

---
+++ geo_test/testcoding.py
@@ -0,0 +1,16 @@
+import json
+import googlemaps
+# api key
+gmaps = googlemaps.Client(key='AIzaSyAcxJTWmScJFuBBsylcl6k4T0_mZST0yto')
+# 좌표입력
+reverse_geocode_result = gmaps.reverse_geocode((36.77519, 126.63483), language='ko')
+#print(reverse_geocode_result)
+with open('users.json', 'w') as f:
+  json.dump(reverse_geocode_result, f)
+
+#저장한 json 파일 불러오기
+with open('users.json', 'r', encoding='UTF8') as f:
+    json_object = json.load(f)
+
+#불러온 json 파일에서 주소만 가져와 출력하기
+print(json_object[0]['formatted_address'])
