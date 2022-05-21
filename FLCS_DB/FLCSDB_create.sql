681402a508867e235189d7c538233030fe89ab50
96-LEEDONGSU <lds961006@naver.com>
2022-05-22 02:45:25 +0900

    FLCS DB의 테이블 변경 : crdnttable의 ID_PK 추가
    processor.py의 코드 정리 및 추가
    : 잡다한 주석과 결과 확인을 위한 print문 삭제 및 최소화, timetable에 시간 insert 코드 추가

--- FLCS_DB/FLCSDB_create.sql
+++ FLCS_DB/FLCSDB_create.sql
@@ -1,3 +1,5 @@
+# < 빠른 주석 해제는 드래그 후 [CTRL + /] >
+
 CREATE DATABASE IF NOT EXISTS FLCSDB;
 USE FLCSDB;
 
@@ -11,28 +13,36 @@ CREATE TABLE IF NOT EXISTS CrdntTable (
     crdnt_X VARCHAR(50),
     crdnt_Y VARCHAR(50),
     DataCrawlingTime DATETIME NULL,
-    FOREIGN KEY(DataCrawlingTime) REFERENCES TimeTable(DataCrawlingTime)
+    ID_PK int AUTO_INCREMENT,
+    FOREIGN KEY(DataCrawlingTime) REFERENCES TimeTable(DataCrawlingTime),
+    PRIMARY KEY(ID_PK)
 );
 
-#아래의 코드로 crdnttable의 형태를 확인하십시오
-#desc crdnttable;
+# 아래의 코드로 crdnttable의 형태를 확인하십시오
+-- desc crdnttable;
 
-select * from crdnttable;
-select * from timetable;
-insert into timetable value ("2022-03-31 23:30:00");
-insert into timetable value ("2022-04-01 01:30:00");
-insert into timetable value ("2022-04-11 06:00:00");
+-- select * from crdnttable;
+-- select * from timetable;
 
 
--- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
--- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
--- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
--- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
--- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
--- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
--- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
--- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
--- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
+# 테스트용 insert
+# timetable에 데이터를 입력할 날짜를 먼저 입력하고, crdnttable에 데이터를 입력합니다.
+# ID_PK는 auto_increment로 자동 입력되니 생략하고 insert하도록 합니다.
 
-    
-#alter table crdnttable add column properties varchar(100) not null;
+-- insert into timetable value ("2022-03-31 23:30:00");
+-- insert into timetable value ("2022-04-01 01:30:00");
+-- insert into timetable value ("2022-04-11 06:00:00");
+
+-- insert into crdnttable(ID, crdnt_X, crdnt_Y, DataCrwlingTime) value (1, 123456789, 789456123, "2022-05-06 09:00:00");
+-- insert into crdnttable(ID, crdnt_X, crdnt_Y, DataCrwlingTime) value (1, 123456789, 789456123, "2022-05-06 09:00:00");
+-- insert into crdnttable(ID, crdnt_X, crdnt_Y, DataCrwlingTime) value (2, 123456789, 789456123, "2022-05-06 09:00:00");
+-- insert into crdnttable(ID, crdnt_X, crdnt_Y, DataCrwlingTime) value (2, 123456789, 789456123, "2022-05-06 09:00:00");
+-- insert into crdnttable(ID, crdnt_X, crdnt_Y, DataCrwlingTime) value (1, 123456789, 789456123, "2022-05-19 12:00:00");
+-- insert into crdnttable(ID, crdnt_X, crdnt_Y, DataCrwlingTime) value (1, 123456789, 789456123, "2022-05-19 12:00:00");
+-- insert into crdnttable(ID, crdnt_X, crdnt_Y, DataCrwlingTime) value (1, 123456789, 789456123, "2022-05-19 15:00:00");
+-- insert into crdnttable(ID, crdnt_X, crdnt_Y, DataCrwlingTime) value (1, 123456789, 789456123, "2022-05-19 15:00:00");
+-- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
+-- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
+-- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
+-- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
+-- insert into crdnttable(crdnt_X, crdnt_Y, DataCrwlingTime) value (123456789, 789456123, "2022-05-06 12:00:00");
--- python_scripts/kml/processor.py
+++ python_scripts/kml/processor.py
@@ -135,9 +135,10 @@ def insert_proc(coor, TBL, coorID):
 def DB_insert(file_name):
     if Dup_pass(file_name)==True:
         global conn, curs, insert_time,db_host,db_user,db_pw,db_name,TBLname
-        insert_time=file_name.split('/')[2].split('\\')[1].replace("-", " 0").replace("_", "-").split('-')
+        insert_time = file_name.split('/')[2].split('\\')[1].replace("-", " 0").replace("_", "-").split('-')
         insert_time = "-".join(insert_time[0:3])
-        print(insert_time)
+        print("사용된 파일 시간 : " + insert_time)
+
         TBLname='crdnttable'
         with open(file_name, "r", encoding="utf8") as file:
             contents = file.read()
@@ -149,34 +150,23 @@ def DB_insert(file_name):
         except:
             print("예외2")
             exit()
-        #json_loads로 불러온 데이터의 coordinates를 불러오면
-        #coor[0]은 해당 좌표 전체
-        #coor[0][i]은 해당 좌표 전체의 i번째 좌표
-        #coor[0][i][j]는 해당 좌표 전체의 i번째 j좌표 (x, y) 각각에 접근가능
+        # <참고> 
+        #   json_loads로 불러온 데이터의 coordinates를 불러오면 coor[0]은 해당 좌표 전체,
+        #   coor[0][i]은 해당 좌표 전체의 i번째 좌표, coor[0][i][j]는 해당 좌표 전체의 i번째 j좌표 (x, y) 각각에 접근가능
         dbinsert_num = 0
+
+        # 최초 해당 시간에 데이터를 입력할 때 timetable에 데이터를 저장하기 위한 SQL문
+        sql = "insert ignore into timetable value (\"" + insert_time + "\")"
+        curs.execute(sql)
+        print(sql)
+        conn.commit()
+
         for i in range(len(json_data["features"])):
             coor = (json_data["features"][i]["geometry"]["coordinates"])
-            prop = (json_data["features"][i]["properties"]) #아래에서 출력하지 않는 것 (공유할 때 생략)
             for k in range(len(coor[0])):
-                print(coor[0][k])
-                print(TBLname)
-                print(i)
-                print("\n")
-                dbinsert_num+=1
-                print("db 삽입 시도 횟수 : " + str(dbinsert_num))
+                dbinsert_num += 1
                 insert_proc(coor[0][k], TBLname, i)
-        #select_sql = "select * from " + TBLname
-        select_sql = "select * from crdnttable"
-        curs.execute(select_sql)
-        result = curs.fetchall()
-        print(result)
-        # coor = (json_data["features"][0]["geometry"]["coordinates"])
-        # with open('test.csv', 'w', newline='') as f:
-        #     for i in range(len(coor[0])):
-        #         for j in range(len(coor[0][i])):
-        #             writer = csv.writer(f)
-        #             writer.writerow(coor[0][i][j])
-        #insert_proc(ROOT_DIR, CSV, TBLname)
+        print("총 DB 삽입 시도 횟수 : " + str(dbinsert_num))
         file.close()
         conn.close()
         exit()
